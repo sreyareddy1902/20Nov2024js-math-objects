89ec36e715da25edb1352c04ee97635f73c6c2c9
sreyareddy1902 <158560047+sreyareddy1902@users.noreply.github.com>
2024-11-20 15:12:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+//math properties
+//Math.PI
+let r=2
+document.write(Math.PI*r*r)
+
+//Math.E
+console.log(Math.E)
+
+//Math.SQRT2
+console.log(Math.SQRT2)
+
+//Math.SQRT1/2
+console.log(Math.SQRT1_2)
+
+//Math.LN2
+console.log(Math.LN2)
+
+//Math.LN10
+console.log(Math.LN10)
+
+//Math.LOG2E
+console.log(Math.LOG2E)
+
+//Math.LOG10E
+console.log(Math.LOG10E)
+
